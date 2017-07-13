1e6fcb27ecbe76ffa81dd64429539a8cc4a211f5
progressively <wagnerruben551@gmail.com>
2017-07-13 10:28:01 -0500

    backbar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-function alertMe(){
-    alert('Thank you your message has been submitted I will be getting back to you shortly!');
+function submitContactForm() {
+    var x = document.forms["ContactForm"]["Email"].value;
+    if (x == "") {
+        alert("Email must be completed!");
+        return false;
+    } else { 
+        alert('Thank you your message has been submitted I will be getting back to you shortly!');
+    }
 }
+
+
+
+
