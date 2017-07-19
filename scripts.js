44f183e0c9fc33cee6ad0a2ca9dc1b11b76f3f2f
progressively <wagnerruben551@gmail.com>
2017-07-19 10:56:01 -0500

    jquery and bootstrap

--- scripts.js
+++ scripts.js
@@ -7,7 +7,20 @@ function submitContactForm() {
         alert('Thank you your message has been submitted I will be getting back to you shortly!');
     }
 }
+var projects = [
+    {
+        "name": "claim academy"
+        'link': "http://claimacademystl.com/"
+    }, {
+        "name":"DexSphere"
+    }
+];
 
+$(document).ready(function() {
 
+$.each(projects, function(index, project){
+    console.log(project);
+    $('#projects').append('<a href"#">' +project.name + '</a>');
+});
 
 
